381d5d27e4c989661111c5d83f8bdc1da1bb4201
Joe Lezala <allaboutthatmace1789@gmail.com>
2019-12-01 15:23:37 -0600

    replace needless use of recursive function with iterate call, improve fuel-one-level

--- src/adventofcode/core.clj
+++ src/adventofcode/core.clj
@@ -1,13 +1,10 @@
 (ns adventofcode.core)
 
 (defn- fuel-one-level [mass]
-  (max 0 (- (int (/ (float mass) 3)) 2)))
-
-(defn- recursive-mass-tree [mass]
-  (cons mass (lazy-seq (recursive-mass-tree (fuel-one-level mass)))))
+  (max 0 (- (quot mass 3) 2)))
 
 (defn fuel-required [mass]
-  (reduce + (rest (take-while #(> % 0) (recursive-mass-tree mass)))))
+  (reduce + (rest (take-while #(> % 0) (iterate fuel-one-level mass)))))
 
 (defn total-fuel-required [module-masses]
   (reduce + (map fuel-required module-masses)))
