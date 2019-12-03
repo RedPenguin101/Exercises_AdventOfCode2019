63b4f45ce90796f68cee4e67cfe19f485acf6a08
Joe Lezala <allaboutthatmace1789@gmail.com>
2019-12-02 18:23:44 -0600

    add intcode load from file

--- src/adventofcode/intcode_emulator.clj
+++ src/adventofcode/intcode_emulator.clj
@@ -8,10 +8,16 @@
         [x y] (map code [x-loc y-loc])]
     (assoc code replace-loc (func x y))))
 
-(defn run-intcode
-  ([input] (run-intcode 0 input))
+(defn run
+  ([input] (run 0 input))
   ([position input]
    (let [operation (input position)]
      (if (= operation 99)
        input
        (recur (+ position 4) (do-operation ({1 + 2 *} operation) position input))))))
+
+(defn load-intcode [filename]
+  (vec (map #(Integer/parseInt %) (clojure.string/split (slurp filename) #", "))))
+
+(defn run-intcode [filename]
+  (run (load-intcode filename)))
--- test/adventofcode/intcode_emulator_test.clj
+++ test/adventofcode/intcode_emulator_test.clj
@@ -7,8 +7,14 @@
   "about the final calculations"
   (fact
     "examples provided"
-    (run-intcode [99]) => [99]
-    (run-intcode [1 0 0 0 99]) => [2 0 0 0 99]
-    (run-intcode [2 3 0 3 99]) => [2 3 0 6 99]
-    (run-intcode [2 4 4 5 99 0]) => [2 4 4 5 99 9801]
-    (run-intcode [1 1 1 4 99 5 6 0 99]) => [30 1 1 4 2 5 6 0 99]))
+    (run [99]) => [99]
+    (run [1 0 0 0 99]) => [2 0 0 0 99]
+    (run [2 3 0 3 99]) => [2 3 0 6 99]
+    (run [2 4 4 5 99 0]) => [2 4 4 5 99 9801]
+    (run [1 1 1 4 99 5 6 0 99]) => [30 1 1 4 2 5 6 0 99]))
+
+(facts
+  "about loading int-code data"
+  (fact
+    "intcode test load"
+    (load-intcode "intcode_test.txt") => [0 1 2 3 4]))
