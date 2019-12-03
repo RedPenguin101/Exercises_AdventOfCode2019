55660d51e3b3df6471144ff3ca1c72af650483c4
Joe Lezala <allaboutthatmace1789@gmail.com>
2019-12-02 18:13:33 -0600

    minor refactor for clarity

--- src/adventofcode/intcode_emulator.clj
+++ src/adventofcode/intcode_emulator.clj
@@ -1,18 +1,17 @@
 (ns adventofcode.intcode-emulator)
 
-(def ops {1 + 2 *})
-
 (defn loc-calc [operation-pos]
   [(+ 1 operation-pos) (+ 2 operation-pos) (+ 3 operation-pos)])
 
-(defn operation [func operation-pos code]
+(defn do-operation [func operation-pos code]
   (let [[x-loc y-loc replace-loc] (map code (loc-calc operation-pos))
         [x y] (map code [x-loc y-loc])]
     (assoc code replace-loc (func x y))))
 
 (defn run-intcode
-  ([code] (run-intcode 0 code))
-  ([start-pos code]
-   (if (= 99 (code start-pos))
-     code
-     (recur (+ 4 start-pos) (operation (ops (code start-pos)) start-pos code)))))
+  ([input] (run-intcode 0 input))
+  ([position input]
+   (let [operation (input position)]
+     (if (= operation 99)
+       input
+       (recur (+ position 4) (do-operation ({1 + 2 *} operation) position input))))))
