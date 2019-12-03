2a2a4a4feab8219654e39a6b4b89e23071b1c4c6
Joe Lezala <allaboutthatmace1789@gmail.com>
2019-12-03 09:11:46 -0600

    path appender with instruction for day3

--- src/adventofcode/manhattan_calc.clj
+++ src/adventofcode/manhattan_calc.clj
@@ -1,1 +1,27 @@
 (ns adventofcode.manhattan-calc)
+
+;; find the intersection point closest to the central port.
+
+;; Manhattan
+;; https://en.wikipedia.org/wiki/Taxicab_geometry
+;; sum of absloute values of pi-qi where and q are vectors
+
+
+;; instruction is U5 etc, path is vec of l2 vectors
+(defn calc-next-coord [coord instruction]
+  (let [[direction mag-string] (re-seq #"[a-zA-Z0-9]" instruction)
+        mag (Integer/parseInt mag-string)]
+    (case direction
+      "U" (vector (first coord) (+ (second coord) mag))
+      "D" (vector (first coord) (- (second coord) mag))
+      "R" (vector (+ (first coord) mag) (second coord))
+      "L" (vector (- (first coord) mag) (second coord)))))
+
+(defn update-path [path instruction]
+  (conj path (calc-next-coord (last path) instruction)))
+
+(defn find-intersections [path1 path2])
+
+(defn manhattan-distance [coord])
+
+
--- test/adventofcode/manhattan_calc_test.clj
+++ test/adventofcode/manhattan_calc_test.clj
@@ -1,2 +1,22 @@
 (ns adventofcode.manhattan-calc-test
-  (:require [clojure.test :refer :all]))
+  (:require [midje.sweet :refer :all]
+            [adventofcode.manhattan-calc :refer :all]))
+
+
+(facts
+  "about coordinate calculator"
+  (fact
+    "any coord with an instruction length of zero should not change"
+    (calc-next-coord [1 1] "U0") => [1 1])
+  (fact
+    "a cood with one up or down should change the y coord by one"
+    (calc-next-coord [1 1] "U1") => [1 2]
+    (calc-next-coord [1 1] "D1") => [1 0])
+  (fact
+    "a cood with one left or right should change the y coord by one"
+    (calc-next-coord [1 1] "R1") => [2 1]
+    (calc-next-coord [1 1] "L1") => [0 1])
+  (fact
+    "a path should be updated with the next instrcution"
+    (update-path [[1 1] [1 2]] "U1") => [[1 1] [1 2] [1 3]])
+  )
