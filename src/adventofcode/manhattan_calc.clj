51387e472acb652a9837880b79b623c1bc8369e9
Joe Lezala <allaboutthatmace1789@gmail.com>
2019-12-03 21:18:07 -0600

    min manhattan distance working

--- src/adventofcode/manhattan_calc.clj
+++ src/adventofcode/manhattan_calc.clj
@@ -8,20 +8,63 @@
 
 
 ;; instruction is U5 etc, path is vec of l2 vectors
-(defn calc-next-coord [coord instruction]
-  (let [[direction mag-string] (re-seq #"[a-zA-Z0-9]" instruction)
-        mag (Integer/parseInt mag-string)]
+(defn calc-end [coord instruction]
+  (let [direction (re-find #"[A-Z]" instruction)
+        mag (Integer/parseInt (re-find #"\d+" instruction))]
     (case direction
       "U" (vector (first coord) (+ (second coord) mag))
       "D" (vector (first coord) (- (second coord) mag))
       "R" (vector (+ (first coord) mag) (second coord))
       "L" (vector (- (first coord) mag) (second coord)))))
 
-(defn update-path [path instruction]
-  (conj path (calc-next-coord (last path) instruction)))
+(defn create-line [start-coord instruction]
+  {:start start-coord :end (calc-end start-coord instruction)})
 
-(defn find-intersections [path1 path2])
+(defn next-line [lines instruction]
+  (let [new-start (:end (last lines))
+        new-end (calc-end new-start instruction)]
+    (conj lines {:start new-start :end new-end})))
 
-(defn manhattan-distance [coord])
+(defn build-path [instructions]
+  (reduce next-line
+          [{:start [0 0] :end (calc-end [0 0] (first instructions))}]
+          (rest instructions)))
 
+(defn is-hor? [line]
+  (= ((:start line) 1) ((:end line) 1)))
+
+(defn intersect? [line1 line2]
+  (let [a1 (:start line1) a2 (:end line1)
+        b1 (:start line2) b2 (:end line2)]
+    (and
+      (not= (> (a1 0) (b1 0)) (> (a2 0) (b2 0)))
+      (not= (> (a1 1) (b1 1)) (> (a2 1) (b2 1))))))
+
+(defn manhattan-distance [vec1 vec2]
+  (reduce + (map #(Math/abs (- %1 %2)) vec1 vec2)))
+
+(defn find-intersection* [line1 line2]
+  (let [hoz-line (if (is-hor? line1) line1 line2)
+        vert-line (if-not (is-hor? line1) line1 line2)
+        x ((:start vert-line) 0)
+        y ((:start hoz-line) 1)]
+    {:intersect [x y] :m-distance (manhattan-distance [0 0] [x y])}))
+
+(defn find-intersection [line1 line2]
+  (cond
+    (apply = (map is-hor? [line1 line2])) nil
+    (intersect? line1 line2) (find-intersection* line1 line2)
+    :else nil))
+
+(defn find-intersections [path1 path2]
+  (remove nil? (for [a path1 b path2] (find-intersection a b))))
+
+(defn min-manhatten-intersection [path1 path2]
+  (apply min (map :m-distance (remove nil? (find-intersections path1 path2)))))
+
+(defn str->instrs [string]
+  (clojure.string/split string #","))
+
+(defn length [line]
+  (manhattan-distance (:start line) (:end line)))
 
--- test/adventofcode/manhattan_calc_test.clj
+++ test/adventofcode/manhattan_calc_test.clj
@@ -4,19 +4,32 @@
 
 
 (facts
-  "about coordinate calculator"
+  "about next coordinate calculator"
   (fact
     "any coord with an instruction length of zero should not change"
-    (calc-next-coord [1 1] "U0") => [1 1])
+    (calc-end [1 1] "U0") => [1 1])
   (fact
     "a cood with one up or down should change the y coord by one"
-    (calc-next-coord [1 1] "U1") => [1 2]
-    (calc-next-coord [1 1] "D1") => [1 0])
+    (calc-end [1 1] "U1") => [1 2]
+    (calc-end [1 1] "D1") => [1 0])
   (fact
     "a cood with one left or right should change the y coord by one"
-    (calc-next-coord [1 1] "R1") => [2 1]
-    (calc-next-coord [1 1] "L1") => [0 1])
-  (fact
-    "a path should be updated with the next instrcution"
-    (update-path [[1 1] [1 2]] "U1") => [[1 1] [1 2] [1 3]])
-  )
+    (calc-end [1 1] "R1") => [2 1]
+    (calc-end [1 1] "L1") => [0 1]))
+
+(facts
+  "about creating lines"
+  (fact "a line creates correctly"
+        (create-line [5 5] "U5") => {:start [5 5] :end [5 10]}))
+
+(facts
+  "about creating paths"
+  (fact "a path creates from a single instruction"
+        (build-path ["U5"]) => [{:start [0 0] :end [0 5]}]))
+
+(facts
+  "about the minimum manhattan distance"
+  (fact "from the problem"
+        (min-manhatten-intersection
+          (build-path (str->instrs "R75,D30,R83,U83,L12,D49,R71,U7,L72"))
+          (build-path (str->instrs "U62,R66,U55,R34,D71,R55,D58,R83"))) => 159))
