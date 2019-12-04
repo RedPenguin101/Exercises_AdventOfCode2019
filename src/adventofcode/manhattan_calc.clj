de9639e5511ca8bafc89d7a71351f51a8f7fd6f8
Joe Lezala <allaboutthatmace1789@gmail.com>
2019-12-03 23:25:45 -0600

    finding intersection

--- src/adventofcode/manhattan_calc.clj
+++ src/adventofcode/manhattan_calc.clj
@@ -1,2 +1,29 @@
 (ns adventofcode.manhattan-calc)
 
+(defn move [current-pos cmd]
+  (let [[x y] current-pos
+        dir (subs cmd 0 1)
+        steps (range 1 (inc (Integer/parseInt (subs cmd 1))))]
+    (for [step steps]
+      (case dir
+        "R" [(+ x step) y]
+        "L" [(- x step) y]
+        "U" [x (+ y step)]
+        "D" [x (- y step)]))))
+
+(defn build-path [cmd-string]
+  (drop
+    1
+    (reduce
+      (fn [old-path cmd]
+        (concat old-path (move (last old-path) cmd)))
+      [[0 0]] (clojure.string/split cmd-string #","))))
+
+(defn find-intersections [path1 path2]
+  (vec (clojure.set/intersection
+         (set path1)
+         (set path2))))
+
+(defn distance [point1 point2]
+  0)
+
--- test/adventofcode/manhattan_calc_test.clj
+++ test/adventofcode/manhattan_calc_test.clj
@@ -2,3 +2,20 @@
   (:require [midje.sweet :refer :all]
             [adventofcode.manhattan-calc :refer :all]))
 
+(facts
+  "about moving"
+  (fact ""
+        (move [0 0] "U5") => '([0 1] [0 2] [0 3] [0 4] [0 5])))
+
+(facts
+  "about path building"
+  (fact
+    (build-path "U1,R1,D1") => '([0 0] [0 1] [1 1] [1 0])))
+
+(facts
+  "about intersections"
+  (fact
+    (find-intersections
+      (build-path "R75,D30,R83,U83,L12,D49,R71,U7,L72")
+      (build-path "U62,R66,U55,R34,D71,R55,D58,R83"))
+    => false))
