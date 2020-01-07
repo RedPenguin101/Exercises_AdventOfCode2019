341c55ef91e2613dddc80dbb398631eefaff0e37
Joe Lezala <allaboutthatmace1789@gmail.com>
2020-01-06 22:38:43 -0600

    got collect working, redid simple to be not async

--- src/adventofcode/intcode_async.clj
+++ src/adventofcode/intcode_async.clj
@@ -3,29 +3,45 @@
             [clojure.core.async :as a
                                 :refer [>!! >! <!! <! chan go close!]]))
 
-(comment
-  "program operations pass the program state around
-  memory is the main thing
-  pointer is the pointer to the current piece of memory under cursor
-  in-chan and out-chan are the channels the computer looks for input
-  and puts its output")
-
-;; this program takes input and immediately passes it out
-
-(defn boot [memory]
+(defn- boot [memory]
   {:memory memory :pointer 0})
+
+(defn deconstruct-opcode-value 
+  "Given a long opcode of up to 5 digits, returns a vector of the param-values
+   (a vector of the first three digits) and the opcode (the last two digits)"
+  [number]
+  (let [param-vals (quot number 100)]
+    [(reverse (map #(Character/digit % 10) (format "%03d" param-vals)))
+     (- number (* param-vals 100))]))
+
+(defn opcode [number]
+  ((deconstruct-opcode-value number) 1))
+
+(defn immediate? [{:keys [memory pointer] :as state} position]
+  (= 1 (nth ((deconstruct-opcode-value (memory pointer)) 0) position)))
 
 (defn calc-new-pos-value [{:keys [memory pointer] :as state}]
   ;(println "calc" state)
-  (({1 + 2 *} (memory pointer)) 
-   (memory (memory (+ pointer 1))) 
-   (memory (memory (+ pointer 2)))))
+  (({1 + 2 *} (opcode (memory pointer))) 
+   ((if (immediate? state 0) identity memory) (memory (+ pointer 1))) 
+   ((if (immediate? state 1) identity memory) (memory (+ pointer 2)))))
+
+(comment "below gives example"
+  (def s {:memory [1002 4 3 4 33] :pointer 0})
+  (def m (:memory s))
+  (def p (:pointer s))
+  
+  ((if (immediate? s 0) identity m) (m (+ p 1))) ;=> 33, because in pos mode
+  ((if (immediate? s 1) identity m) (m (+ p 2))) ;=> 3 in immediate mode
+  (calc-new-pos-value s) ;=> 99
+  )
 
 (defn do-operation [{:keys [pointer, memory] :as state}]
   ;(println "op" state)
   (-> state
       (assoc :pointer (+ 4 pointer))
-      (assoc-in [:memory (memory (+ 3 pointer))] (calc-new-pos-value state))))
+      (assoc-in [:memory (memory (+ 3 pointer))] 
+                (calc-new-pos-value state))))
 
 (defn process-input [{:keys [pointer, memory] :as state} input]
   ;(println "in" state "input" input)
@@ -37,41 +53,63 @@
   ;(println "out" state)
   (memory (memory (+ 1 pointer))))
 
+(defn run-singly [{:keys [pointer memory] :as state} inputs outputs]
+  (cond
+    (= 99 (opcode (memory pointer))) state
+
+    (= 3 (opcode (memory pointer))) 
+      (recur (process-input state (first inputs)) (drop 1 inputs) outputs)
+
+    (= 4 (opcode (memory pointer))) 
+      (recur (assoc state :pointer (+ 2 pointer)) 
+             inputs 
+             (conj outputs (process-output state)))
+
+    :else (recur (do-operation state) inputs outputs)))
+
 (defn run [{:keys [pointer memory] :as state} in]
   (let [out (chan) 
         final (chan (a/sliding-buffer 1))]
     (a/go-loop [{:keys [pointer memory] :as state} state]
       (cond
-        (= 99 (memory pointer)) (do  (>! final state) (close! final) (close! out))
-        (= 3 (memory pointer)) (recur (process-input state (<! in)))
-        (= 4 (memory pointer)) (do (>! out (process-output state))
+        (= 99 (opcode (memory pointer))) (do (>! final state) (close! final) (close! out))
+        (= 3 (opcode (memory pointer))) (recur (process-input state (<! in)))
+        (= 4 (opcode (memory pointer))) (do (>! out (process-output state))
                                    (recur (assoc state :pointer (+ 2 pointer))))
         :else (recur (do-operation state))))
     [out final]))
 
-(defn simple-run [memory]
-  (let [[_ final] (run (boot memory) nil)]
-  (<!! final)))
+(defn simple-run [memory & inputs]
+  (run-singly (boot memory) (when inputs inputs) []))
 
-(def in-out-mem [3 5 4 5 99 -1])
+(defn- take-until-closed 
+  "Given a channel, takes messages from that channel until it is closed
+  then returns a sequence of all messages it receives"
+  ([channel] (take-until-closed [] channel))
+  ([result-seq channel]
+   (let [message (<!! channel)]
+     (if message 
+       (recur (conj result-seq message) channel)
+       result-seq))))
 
 (defn collect-output [memory input] 
   (let [in (chan) 
-       [out _] (run (boot in-out-mem) in)]
-    (>!! in input)
-    (take 1 out)))
-
-(def adder [3 10 1 9 10 10 4 10 99 1 -1])
-
+        [out] (run (boot memory) in)]
+    (when input (>!! in input))
+    (take-until-closed out)))
 
 (comment
-
-  (collect-output adder 10)
-
   (def in-mem [1 1 3 5 99 -1])
   (def in-out-mem [3 5 4 5 99 -1])
   (def immediate-halt-mem [99])
-  
+  (def return-two-things [4 5 4 6 99 123 321])
+  (def adder [3 10 1 9 10 10 4 10 99 1 -1])
+
+  (collect-output adder 10)
+  (collect-output return-two-things nil)
+  (collect-output immediate-halt-mem 10)
+  (simple-run in-mem)
+
   (let [in (chan) 
         o1 (run (boot in-out-mem) in)
         out (run (boot in-out-mem) o1)]
@@ -79,11 +117,15 @@
     (<!! out))
   
   (let [in (chan) 
-        middle (run (boot adder) in)
-        out (run (boot adder) middle)]
+        [middle] (run (boot adder) in)
+        [out] (run (boot adder) middle)]
     (>!! in 5)
     (<!! out))  
+
+  (let [in (chan)
+        [out] (run (boot adder) in)]
+    (>!! in 5)
+    (take-until-closed out))
   
-  
-  (simple-run in-mem))
+  )
 
--- test/adventofcode/intcode_async_test.clj
+++ test/adventofcode/intcode_async_test.clj
@@ -2,12 +2,28 @@
   (:require [adventofcode.intcode-async :refer :all]
             [midje.sweet :refer :all]))
 
-(fact "test add and mult from day 2"
+(fact "add and mult from day 2"
   (:memory (simple-run [1,0,0,0,99])) => [2 0 0 0 99]
   (:memory (simple-run [2,3,0,3,99])) => [2 3 0 6 99]
   (:memory (simple-run [2 4 4 5 99 0])) => [2 4 4 5 99 9801]
   (:memory (simple-run [1 1 1 4 99 5 6 0 99])) => [30 1 1 4 2 5 6 0 99])
 
 (fact "input and outputs from day 5"
+  (collect-output [4 5 4 6 99 123 321] nil) => [123 321])
+
+(fact "long opcode desconstruction"
+  (deconstruct-opcode-value 1002) => [[0 1 0] 2])
+
+(fact "immediate"
+  (immediate? {:memory [1002] :pointer 0} 0) => false
+  (immediate? {:memory [1002] :pointer 0} 1) => true
+  (immediate? {:memory [1002] :pointer 0} 2) => false)
+
+(fact "calc new position value in immediate mode"
+  (calc-new-pos-value {:memory [1002 4 3 4 33] :pointer 0}) => 99)
+
+(fact "immediate mode for multiply and add"
+  (:memory (simple-run [1002 4 3 4 33])) => [1002 4 3 4 99]
+  (:memory (simple-run [1001 4 3 4 96])) => [1001 4 3 4 99]
   )
 
