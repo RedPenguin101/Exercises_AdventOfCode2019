ccebcd8c17aff89929809676637c401b9b50f77c
RedPenguin101 <allaboutthatmace1789@gmail.com>
2020-01-17 22:33:58 -0600

    day 10 part 2

--- src/adventofcode/asteroids.clj
+++ src/adventofcode/asteroids.clj
@@ -1,9 +1,9 @@
 (ns adventofcode.asteroids)
 
-(defn- get-input [string]
+(defn get-input [string]
   (remove #(= "\n" %) (clojure.string/split string #"")))
 
-(defn- asteroid-co-ords 
+(defn asteroid-co-ords 
   "Given a sequence of '#' and other things indicating space, returns a sequence of
    co-ordinates of all asteroids"
   [input width height] 
@@ -24,30 +24,8 @@
 (defn- gcd-on-point [[a b :as point]]
   (let [gcd (Math/abs (gcd point))]
     (if 
-      (= 0 a b) [0 0] 
+      (= 0 a b) [0 0 0] 
       [(/ a gcd) (/ b gcd) gcd])))
-
-(defn- add-sweep-number [points]
-  )
-
-(map #(calc-direction % [3 4]) 
-     (asteroid-co-ords (get-input (slurp "resources/inputday10_test.txt")) 5 5))
-
-(def x (->> (asteroid-co-ords (get-input (slurp "resources/inputday10_test.txt")) 5 5)
-            (map #(calc-direction % [3 4]))
-            div-by-gcd))
-
-(defn- add-pos-in-line* [co-ords]
-  (map #(conj %1 %2) 
-       (sort-by #(nth % 2) co-ords) 
-       (range (count co-ords))))
-
-(defn- add-pos-in-line [co-ords]
-  (->> co-ords
-       (group-by #(vector (first %) (second %)))
-       vals
-       (map #(add-pos-in-line* %))
-       (apply concat)))
 
 (defn- div-by-gcd [seq-of-points]
   (map #(gcd-on-point %) seq-of-points))
@@ -76,3 +54,49 @@
       (asteroid-co-ords width height)
       count-visible-asteroids
       find-max-visible*))
+
+(defn- add-pos-in-line* [co-ords]
+  (map #(conj %1 %2)
+       (sort-by #(nth % 2) co-ords)
+       (range (count co-ords))))
+
+(defn- add-sweep-number [co-ords]
+  (->> co-ords
+       (group-by #(vector (first %) (second %)))
+       vals
+       (map #(add-pos-in-line* %))
+       (apply concat)))
+
+(defn- calc-angle [[a b]]
+  (+ 180 (* -1 (* (/ 180 Math/PI) (Math/atan2 a b)))))
+
+(defn- laser-order [co-ords]
+  (->> co-ords
+       add-sweep-number
+       (map #(conj % (calc-angle %)))
+       (sort-by #(nth % 4))
+       (sort-by #(nth % 3))))
+
+(defn- relative-coords-with-gcd [coords ref-point]
+  (->> coords 
+       (map #(calc-direction ref-point %))
+       (remove #(= [0 0] %))
+       div-by-gcd))
+
+(defn nth-lasered-from [n co-ords ref-point]
+  (let [point (nth (laser-order (relative-coords-with-gcd co-ords ref-point)) (dec n))]
+    [(+ (ref-point 0) (* (point 0) (point 2)))
+     (+ (ref-point 1) (* (point 1) (point 2)))]))
+
+
+(nth-lasered-from 10 (asteroid-co-ords (get-input (slurp "resources/inputday10_test5.txt")) 20 20) [11 13])
+
+(comment
+  (find-max-visible (slurp "resources/inputday10.txt") 26 26)
+  ;; => {:point [13 17], :count 269}
+  
+  (nth-lasered-from 200 
+                    (asteroid-co-ords (get-input (slurp "resources/inputday10.txt")) 26 26) 
+                    [13 17])
+  ;; => [6 12]
+  )
--- test/adventofcode/asteroids_test.clj
+++ test/adventofcode/asteroids_test.clj
@@ -10,3 +10,18 @@
  (find-max-visible (slurp "resources/inputday10_test5.txt") 20 20) => {:point [11 13] :count 210}
 
  )
+
+(def asts (asteroid-co-ords (get-input (slurp "resources/inputday10_test5.txt")) 20 20))
+
+(fact
+ (nth-lasered-from 1 asts [11 13]) => [11 12]
+ (nth-lasered-from 2 asts [11 13]) => [12 1]
+ (nth-lasered-from 3 asts [11 13]) => [12 2]
+ (nth-lasered-from 10 asts [11 13]) => [12 8]
+ (nth-lasered-from 20 asts [11 13]) => [16 0]
+ (nth-lasered-from 50 asts [11 13]) => [16 9]
+ (nth-lasered-from 100 asts [11 13]) => [10 16]
+ (nth-lasered-from 199 asts [11 13]) => [9 6]
+ (nth-lasered-from 200 asts [11 13]) => [8 2]
+ (nth-lasered-from 201 asts [11 13]) => [10 9]
+ (nth-lasered-from 299 asts [11 13]) => [11 1])
