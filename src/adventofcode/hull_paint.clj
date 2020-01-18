1a9b9518ab5e23a1a4eb5eb520e4d0b6e9277ba7
RedPenguin101 <allaboutthatmace1789@gmail.com>
2020-01-18 13:45:10 -0600

    add update surface and run-robot

--- src/adventofcode/hull_paint.clj
+++ src/adventofcode/hull_paint.clj
@@ -1,5 +1,6 @@
 (ns adventofcode.hull-paint
-  (:require [adventofcode.intcode-async :refer [run-async]]))
+  (:require [adventofcode.intcode-async :refer [run-async]]
+            [clojure.core.async :refer [chan <!! >!!]]))
 
 (comment
   "a surface is a vector of vectors which represent the path of the
@@ -7,22 +8,37 @@
   The vector is ordered, so in the case the robot has visitied the point twice,
   later one will be the return value")
 
-(defn- lookup-color [surface [x y :as point]]
-  (if ((set (map drop-last surface)) point)
-    ((last (filter #(and (= x (% 0)) (= y (% 1))) surface)) 2)
-    0))
+(defn- new-dir [curr-dir turn]
+  (mod (+ curr-dir (if (zero? turn) -1 1)) 4))
 
-(comment
-  (lookup-color [[1 1 0] [1 2 1] [1 1 1]] [1 1])
-  ;; => 1
-  "returns 0 (black) if the co-ordinate isn't there"
-  (lookup-color [[1 1 0] [1 2 1] [1 1 1]] [2 1])
-  ;; => 0
-  )
-
-(defn- new-coord [[x y] curr-dir instr]
-  (case (mod (+ curr-dir (if (zero? instr) -1 1)) 4)
+(defn- new-coord [[x y] curr-dir turn]
+  (case (new-dir curr-dir turn)
     0 [(inc x) y]
     1 [x (inc y)]
     2 [(dec x) y]
     3 [x (dec y)]))
+
+(defn- update-surface [surface [x y] color]
+  (conj surface [x y color]))
+
+(defn run-robot [surface dir in out]
+  (let [color (<!! out)
+        turn (<!! out)
+        new-surface (update-surface surface (new-coord ((last surface) 2) dir turn) color)]
+    (if color
+      (do (>!! in ((last new-surface) 2))
+          (recur new-surface (new-dir dir turn) in out))
+      surface)))
+
+(comment 
+  (def program 
+    (vec (map #(bigint %)
+              (->  "resources/inputday11.txt"
+                   slurp
+                   clojure.string/trim
+                   (clojure.string/split #",")))))
+
+  (let [in (chan)
+        [out] (run-async program in)]
+    (run-robot [0 0 0] 0 in out))
+  )
