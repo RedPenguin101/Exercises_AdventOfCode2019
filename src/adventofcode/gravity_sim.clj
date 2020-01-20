d41dd09e2dcd50846012f6c10475afd6ad26ebbd
RedPenguin101 <allaboutthatmace1789@gmail.com>
2020-01-20 16:44:45 -0600

    make private funcs, delete priv tests

--- src/adventofcode/gravity_sim.clj
+++ src/adventofcode/gravity_sim.clj
@@ -4,27 +4,24 @@
   "A body is represented as a map of position vector and velocity vector"
   {:position [0 0 0] :velocity [1 1 1]})
 
-(defn update-position [{:keys [position velocity] :as body}]
+(defn- update-position [{:keys [position velocity] :as body}]
   (assoc body :position (vec (map #(+ %1 %2) position velocity))))
 
-(defn vel-modifier [a b]
+(defn- vel-modifier [a b]
   (cond
     (zero? (- a b)) identity
     (pos? (- a b))  dec
     (neg? (- a b))  inc))
 
 
-(defn update-velocity 
-  "Given two bodies, modifies the velocity vector of th first one to reflect
-  the impact of gravity between the two bodies"
-  [body1 body2]
+(defn- update-velocity [body1 body2]
   (assoc body1 
          :velocity 
          (vec (map #(%1 %2) 
                    (map #(vel-modifier %1 %2) (:position body1) (:position body2)) 
                    (:velocity body1)))))
 
-(defn update-velocities [bodies]
+(defn- update-velocities [bodies]
   (for [body bodies]
     (reduce update-velocity body bodies)))
 
