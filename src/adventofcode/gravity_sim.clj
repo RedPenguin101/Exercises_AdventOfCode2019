52c78ea36031b881d524671798deeb416e0f688b
RedPenguin101 <allaboutthatmace1789@gmail.com>
2020-01-20 16:22:41 -0600

    updating velocity

--- src/adventofcode/gravity_sim.clj
+++ src/adventofcode/gravity_sim.clj
@@ -7,4 +7,31 @@
 (defn update-position [{:keys [position velocity] :as body}]
   (assoc body :position (map #(+ %1 %2) position velocity)))
 
+(defn vel-modifier [a b]
+  (let [test (- a b)]
+    (cond
+      (zero? test) identity
+      (pos? test)  dec
+      (neg? test)  inc)))
 
+
+(defn update-velocity 
+  "return vec of functions to apply to a velocity vector"
+  [body1 body2]
+  (assoc body1 :velocity (map #(%1 %2) 
+                              (map #(vel-modifier %1 %2) (:position body1) (:position body2)) 
+                              (:velocity body1))))
+
+(comment
+  (update-velocity {:position [0 0 0] :velocity [0 0 0]}
+                   {:position [1 0 0] :velocity [0 0 0]})
+  ;; => (1 0 0)
+
+  )
+
+(defn update-velocities [bodies]
+  (for [body bodies]
+    (reduce update-velocity body bodies)))
+
+(def bodies [{:position [0 0 0] :velocity [0 0 0]}
+             {:position [1 0 0] :velocity [0 0 0]}])
--- test/adventofcode/gravity_sim_test.clj
+++ test/adventofcode/gravity_sim_test.clj
@@ -6,4 +6,32 @@
  "about update position"
  (update-position {:position [0 0 0] :velocity [1 1 1]}) => {:position [1 1 1] :velocity [1 1 1]})
 
-(fact)
+(facts
+ "about updating velocity"
+ 
+ (fact
+  "when there is only one body, there's no change"
+  (update-velocities [{:position [0 0 0] :velocity [0 0 0]}])
+  => [{:position [0 0 0] :velocity [0 0 0]}])
+ 
+ (fact 
+  "when the the two bodies are in the same place, there is no change"
+  (update-velocities [{:position [0 0 0] :velocity [0 0 0]}
+                      {:position [0 0 0] :velocity [0 0 0]}]) 
+  => [{:position [0 0 0] :velocity [0 0 0]}
+      {:position [0 0 0] :velocity [0 0 0]}]
+  
+  (update-velocities [{:position [0 0 0] :velocity [1 1 1]}
+                      {:position [0 0 0] :velocity [-1 -1 -1]}]) 
+  
+  => [{:position [0 0 0] :velocity [1 1 1]}
+      {:position [0 0 0] :velocity [-1 -1 -1]}])
+
+ (fact
+  "when two bodies are in different places on the x-axis, their velocities are updated"
+  (update-velocities [{:position [0 0 0] :velocity [0 0 0]}
+                      {:position [1 0 0] :velocity [0 0 0]}])
+  => [{:position [0 0 0] :velocity [1 0 0]}
+      {:position [1 0 0] :velocity [-1 0 0]}])
+
+ )
