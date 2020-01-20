d1d7a52dcf7d911d47a0aff08dc839055a975cbe
RedPenguin101 <allaboutthatmace1789@gmail.com>
2020-01-20 17:31:22 -0600

    refactor, add scratch

--- src/adventofcode/gravity_sim.clj
+++ src/adventofcode/gravity_sim.clj
@@ -13,29 +13,36 @@
     (pos? (- a b))  dec
     (neg? (- a b))  inc))
 
-
 (defn- update-velocity [body1 body2]
-  (assoc body1 
-         :velocity 
-         (vec (map #(%1 %2) 
-                   (map #(vel-modifier %1 %2) (:position body1) (:position body2)) 
-                   (:velocity body1)))))
+  (assoc 
+   body1 
+   :velocity 
+   (vec (map #(%1 %2) 
+             (map #(vel-modifier %1 %2) 
+                  (:position body1) 
+                  (:position body2)) 
+             (:velocity body1)))))
 
 (defn- update-velocities [bodies]
-  (doall (for [body bodies]
-           (reduce update-velocity body bodies))))
+  (doall ;nessessary to prevent stackoverflow
+   (for [body bodies]
+     (reduce update-velocity body bodies))))
 
 (defn step [bodies]
   (->> (update-velocities bodies)
-      (map update-position)))
+       (map update-position)))
 
 (defn steps [bodies times]
   (if (= times 0)
     bodies
     (recur (step bodies) (dec times)))) 
 
+(defn- sum-of-abs [vector]
+  (apply + (map #(Math/abs %) vector)))
+
 (defn total-energy [{:keys [position velocity]}]
-  (* (apply + (map #(Math/abs %) position)) (apply + (map #(Math/abs %) velocity))))
+  (* (sum-of-abs position) 
+     (sum-of-abs velocity)))
 
 (comment 
   (reduce + (map total-energy [{:position [2 1 -3], :velocity [-3 -2 1]}
@@ -44,6 +51,11 @@
                                {:position [2 0 4], :velocity [1 -1 -1]}]))
   ;; => 179
   )
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;
+;; getting input
+;;;;;;;;;;;;;;;;;;;;;;;;
 
 (def input-pattern #"<x=(-?\d+), y=(-?\d+), z=(-?\d+)>")
 
