9cda171192f045620a1ffa78faef040cf2b66868
RedPenguin101 <allaboutthatmace1789@gmail.com>
2020-01-20 16:43:08 -0600

    add step

--- src/adventofcode/gravity_sim.clj
+++ src/adventofcode/gravity_sim.clj
@@ -5,7 +5,7 @@
   {:position [0 0 0] :velocity [1 1 1]})
 
 (defn update-position [{:keys [position velocity] :as body}]
-  (assoc body :position (map #(+ %1 %2) position velocity)))
+  (assoc body :position (vec (map #(+ %1 %2) position velocity))))
 
 (defn vel-modifier [a b]
   (cond
@@ -27,3 +27,24 @@
 (defn update-velocities [bodies]
   (for [body bodies]
     (reduce update-velocity body bodies)))
+
+(defn step [bodies]
+  (->> (update-velocities bodies)
+      (map update-position)))
+
+(comment
+  "take multiple steps like"
+
+  (def bodies [{:position [-1 0 2] :velocity [0 0 0]}
+               {:position [2 -10 -7] :velocity [0 0 0]}
+               {:position [4 -8 8] :velocity [0 0 0]}
+               {:position [3 5 -1] :velocity [0 0 0]}])
+
+  "this would give the positions after 10 steps"
+  (last (take 11 (iterate step bodies)))
+  ;; => ({:position (2 1 -3), :velocity [-3 -2 1]}
+  ;;     {:position (1 -8 0), :velocity [-1 1 3]}
+  ;;     {:position (3 -6 1), :velocity [3 2 -3]}
+  ;;     {:position (2 0 4), :velocity [1 -1 -1]})
+
+  )
--- test/adventofcode/gravity_sim_test.clj
+++ test/adventofcode/gravity_sim_test.clj
@@ -61,8 +61,19 @@
                      {:position [2 -10 -7] :velocity [0 0 0]}
                      {:position [4 -8 8] :velocity [0 0 0]}
                      {:position [3 5 -1] :velocity [0 0 0]}])
- => [ {:position [1 0 2] :velocity [3 -1 -1]}
+ => [{:position [1 0 2] :velocity [3 -1 -1]}
      {:position [2 -10 -7] :velocity [1 3 3]}
      {:position [4 -8 8] :velocity [-3 1 -3]}
      {:position [3 5 -1] :velocity [-1 -3 1]}])
  )
+
+(facts
+ "about stepping through time"
+ (step [{:position [-1 0 2] :velocity [0 0 0]}
+        {:position [2 -10 -7] :velocity [0 0 0]}
+        {:position [4 -8 8] :velocity [0 0 0]}
+        {:position [3 5 -1] :velocity [0 0 0]}])
+ => [{:position [2 -1 1] :velocity [3 -1 -1]}
+     {:position [3 -7 -4] :velocity [1 3 3]}
+     {:position [1 -7 5] :velocity [-3 1 -3]}
+     {:position [2 2 0] :velocity [-1 -3 1]}])
