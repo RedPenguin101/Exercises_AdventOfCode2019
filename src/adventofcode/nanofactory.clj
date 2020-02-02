32e7bd86b1fc9bb9333053a16829df901e3472a1
RedPenguin101 <allaboutthatmace1789@gmail.com>
2020-02-01 19:44:01 -0600

    start again

--- src/adventofcode/nanofactory.clj
+++ src/adventofcode/nanofactory.clj
@@ -29,37 +29,3 @@
     (/ required yields)
     (/ (+ required (- yields (mod required yields))) yields)))
 
-(def recipies
-  {:A {:yields 1 :inputs [[9 :ORE]]}
-   :B {:yields 2 :inputs [[3 :ORE]]}
-   :A2 {:yields 1 :inputs [[1 :A] [1 :ORE]]}})
-
-(defn deconstruct [[required ing-name] recipies]
-  (if (= ing-name :ORE)
-    required
-    (let [ingredients (:inputs (ing-name recipies))]
-      (* (required-batches required (:yields (ing-name recipies)))
-         (reduce + (map #(deconstruct % recipies) ingredients))))))
-
-(defn ore-amount [ing recipies]
-  (if (empty? ing)
-    nil
-    (deconstruct ing recipies)))
-
-(comment 
-  "new representation: recipes are represented as before
-  but now the algo acts on a state that looks like this"
-  {:A [4 10] :B [4 7]}
-  "where the hashmap values are the number of those chemicals
-  the first value is the number actually needed and the second
-  is the number produced (i.e. the difference between them is the slack)")
-
-(defn- calc-ore-req [production recipies]
-  (if (= [:ORE] (keys production)) 
-    ((:ORE production) 0)
-    18))
-
-(defn ore-amount2 [[number requirement] recipies]
-  (calc-ore-req {requirement [number number]} recipies))
-
-(keys {:hello 0 "world" 123})
--- test/adventofcode/nanofactory_test.clj
+++ test/adventofcode/nanofactory_test.clj
@@ -9,21 +9,6 @@
    :B2 {:yields 2 :inputs [[2 :B] [2 :ORE]]}
    :AB {:yields 3 :inputs [[3 :A] [4 :B]]}})
 
-(fact
- ;(ore-amount [] test-recipies) => nil
- (ore-amount2 [1 :ORE] test-recipies) => 1
- (ore-amount2 [5 :ORE] test-recipies) => 5
- (ore-amount2 [2 :A] test-recipies) => 18
- (ore-amount [3 :A] test-recipies) => 27
- (ore-amount [1 :B] test-recipies) => 3
- (ore-amount [2 :B] test-recipies) => 3
- (ore-amount [1 :A2] test-recipies) => 10
- (ore-amount [2 :B2] test-recipies) => 5
- (ore-amount [2 :B2] test-recipies) => 5
- (ore-amount [3 :B2] test-recipies) => 10
- (ore-amount [3 :AB] test-recipies) => 33
- )
-
 
 (def test-recipies2
   {:A {:yields 2 :inputs [[9 :ORE]]}
@@ -34,9 +19,3 @@
    :CA {:yields 1 :inputs [[4 :C] [1 :A]]}
    :FUEL {:yields 1 :inputs [[2 :AB] [3 :BC] [4 :CA]]}
    })
-
-(future-fact
- (ore-amount [10 :A] test-recipies2) => 45
- (ore-amount [24 :B] test-recipies2) => 64
- (ore-amount [1 :FUEL] test-recipies2) => 165
- )
